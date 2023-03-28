a0c6bc3f3248c918f5f38f10b7a7858c50ed8939
rebecastefhani <rebecastefhani@hotmail.com>
2023-03-28 10:47:39 -0300

    commit initial

---
+++ Dio Trilha Java Basico/src/MyClass.java
@@ -0,0 +1,10 @@
+
+public class MyClass {
+
+	public static void main(String[] args) throws Exception {
+		double salarioMinimo = 2500;
+		
+		
+	final double VALOR_DE_PI = 3.14;
+	}
+}
---
+++ Dio Trilha Java Basico/src/Operadores.java
@@ -0,0 +1,40 @@
+
+public class Operadores {
+	
+	public static void main(String[] args) {
+		String nomeCompleto = "LINGUAGEM " + "JAVA";
+		
+		System.out.println(nomeCompleto);
+		
+		int numero = 5;
+		
+		System.out.println( numero -- );
+		
+		System.out.println(numero);
+		
+		boolean variavel = true;
+		
+		System.out.println(!variavel);
+		
+		int a, b;
+		
+		a = 5;
+		b = 6;
+		/*String resultado = "";
+		if(a==b)
+			resultado = "verdadeiro";
+		else
+			resultado = "falso";
+		
+		System.out.println(resultado);
+		
+		forma abreviada abaixo*/
+		
+		String resultado = a==b ?"verdadeiro" : "falso";
+		
+		System.out.println(resultado); 
+		
+		/* .quals quando se trata de texto*/ 
+		}
+
+}
